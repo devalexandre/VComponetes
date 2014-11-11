51b9691fe087b576c52c493f55e597df3f14dfb5
ale-php <alexandre@progs.net.br>
2014-11-10 22:58:25 -0200

    novos metodos

--- PWindow/PWindows.class.php
+++ PWindow/PWindows.class.php
@@ -5,13 +5,21 @@
 
 class PWindows extends TElement{
 
+private $modal;
+private $title;
+
 function __construct(){
 
 parent::__construct('div');
 $this->id = 'windos'.uniqid();
+$this->width = null;
+$this->height = null;
+$this->title = null;
 
+}
 
-
+public function setTitle($title){
+$this->title = ",title: '$title'";
 }
 
 public function setSize($width,$height){
@@ -25,21 +33,32 @@ public function addContent($contente){
 
 
        parent::add($contente);
-     
+  
+}
+
+public  function setModal($modal = false){
+
+$this->modal = ",modal: $modal";
 
 }
 
 public function show(){
 
-if(($this->height == null) || ($this->width)){
+if(($this->height == null) || ($this->width == null)){
 
 throw new Exception('size não especificado');
 }
+
+if($this->height == null){
+
+throw new Exception('title não especificado');
+}
+
 $script = new TElement('script');
 $script->type = 'text/javascript';
 
 $code = "$(function() {
-$('#$this->id').dialog({ minWidth: $this->width,minHeight: $this->height } );
+$('#$this->id').dialog({ minWidth: $this->width,minHeight: $this->height $this->modal $this->title} );
 });";
 
 $script->add($code);
