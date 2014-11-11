9d6ac9fc31dce9f5fe9efe1fd96f959a364fc598
ale-php <alexandre@progs.net.br>
2014-11-10 22:41:15 -0200

    com setSize

--- PWindow/PWindows.class.php
+++ PWindow/PWindows.class.php
@@ -9,40 +9,37 @@ function __construct(){
 
 parent::__construct('div');
 $this->id = 'windos'.uniqid();
-$this->width = '800';
-$this->height = '400';
 
+
+
+}
+
+public function setSize($width,$height){
+
+$this->width = $width;
+$this->height = $height;
 
 }
 
 public function addContent($contente){
 
-if (ob_get_level() == 0) ob_start();
-
-for ($i = 0; $i <= 10 ; $i++){
 
        parent::add($contente);
-        echo str_pad('',4096)."\n";   
-
-        ob_flush();
-        flush();
-        sleep(2);
-}
-
-
-
-ob_end_flush();
-
+     
 
 }
 
 public function show(){
 
+if(($this->height == null) || ($this->width)){
+
+throw new Exception('size não especificado');
+}
 $script = new TElement('script');
 $script->type = 'text/javascript';
 
 $code = "$(function() {
-$('#$this->id').dialog({ minWidth: 800,minHeight: 400 } );
+$('#$this->id').dialog({ minWidth: $this->width,minHeight: $this->height } );
 });";
 
 $script->add($code);
